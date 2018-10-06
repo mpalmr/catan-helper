b61629f74b29c2551b7ab11a2e5369c41ba028c4
Matthew Palmer <matthew@compass.digital.io>
2018-10-06 01:07:00 -0400

    Settlement component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Settlement resource="wood"/>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Settlement from './components/Settlement.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Settlement,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Settlement.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="settlement">
+    <img :src="imgSrc" :alt="imgAlt">
+
+    <button @click="addResource">
+      Add Resource
+    </button>
+
+    <ul v-for="resource in resources" :key="resource">
+      <li>
+        {{ resource }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+// import Resource from './Resource.vue';
+
+export default {
+  name: 'settlement',
+  props: {
+    isCity: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      resources: [],
+    };
+  },
+
+  methods: {
+    addResource() {
+      console.log(this);
+      this.resources.push({
+        type: null,
+        number: null,
+      });
+    },
+  },
+
+  computed: {
+    imgSrc() {
+      const img = this.isCity ? 'city' : 'town';
+      return `/assets/${img}.png`;
+    },
+
+    imgAlt() {
+      return this.isCity ? 'City' : 'Town';
+    },
+  },
+
+  // Components: {
+  //   Resource,
+  // },
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
